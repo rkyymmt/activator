e3e617a8eaec36cc7e5a95c888269f2f9e87d800
Josh Suereth <joshua.suereth@gmail.com>
2012-12-14 14:13:04 -0500

    Migrated to properties file based on Mark's review.
    Added a really lame java class for loading and dole-ing out properties in a scala-friendly fashion.

--- project/Properties.scala
+++ project/Properties.scala
@@ -7,23 +7,33 @@ object Properties {
   val makePropertiesSource = TaskKey[Seq[File]]("make-properties-source")
 
   def makePropertyClassSetting(sbtVersion: String): Seq[Setting[_]] = Seq(
-    sourceGenerators in Compile <+= makePropertiesSource,
-    makePropertiesSource <<= (version, sourceManaged in Compile) map { (v, dir) =>
+    resourceGenerators in Compile <+= makePropertiesSource,
+    makePropertiesSource <<= (version, resourceManaged in Compile, compile in Compile) map { (v, dir, analysis) =>
+      import java.util.{Date, TimeZone}
+	  val formatter = new java.text.SimpleDateFormat("yyyyMMdd'T'HHmmss")
+	  formatter setTimeZone TimeZone.getTimeZone("GMT")
+	  val timestamp = formatter.format(new Date)
       val parent= dir / "snap" / "properties"
       IO createDirectory parent
-      val target = parent / "SnapProperties.java"
-      IO.write(target, makeJavaPropertiesString(v, sbtVersion))
+      val target = parent / "snap.properties"
+      if(!target.exists || target.lastModified < lastCompilationTime(analysis)) {
+        IO.write(target, makeJavaPropertiesString(v, sbtVersion))
+      }
       Seq(target)
     }
   )
 
+  
+  def lastCompilationTime(analysis: sbt.inc.Analysis): Long = {
+    val times = analysis.apis.internal.values map (_.compilation.startTime)
+    if(times.isEmpty) 0L else times.max
+  }
+  
 
   def makeJavaPropertiesString(version: String, sbtVersion: String): String =
-    """|package snap.properties;
-       |
-       |public class SnapProperties {
-       |   public static String APP_VERSION = "%s";
-       |   public static String SBT_VERSION = "%s";
-       |}
+    """|app.version=%s
+       |sbt.version=$s
        |""".stripMargin format (version, sbtVersion)
+  
+  
 }
