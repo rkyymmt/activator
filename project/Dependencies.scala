c55e13b7ead849c68d41c66fa24bebb6d8b0d5f0
Josh Suereth <joshua.suereth@gmail.com>
2012-12-17 15:17:21 -0500

    Merged sbt-child projects into the main snap build.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,12 +5,19 @@ import Keys._
 object SnapDependencies {
   val sbtVersion = "0.12.1"
 
-  val sbtLauncherInterface = "org.scala-sbt" % "launcher-interface" % sbtVersion % "provided"
-
-  val webjarsPlay         = "org.webjars" % "webjars-play" % "2.0"
-  val webjarsBootstrap    = "org.webjars" % "bootstrap" % "2.2.1"
-  val commonsIo           = "commons-io" % "commons-io" % "2.0.1"
-
+  val sbtLauncherInterface = "org.scala-sbt" % "launcher-interface" % sbtVersion
+  val sbtMain              = "org.scala-sbt" % "main" % sbtVersion
+  val sbtTheSbt            = "org.scala-sbt" % "sbt" % sbtVersion
+  val sbtIo                = "org.scala-sbt" % "io" % sbtVersion
+  val sbtLogging           = "org.scala-sbt" % "logging" % sbtVersion
+  val sbtProcess           = "org.scala-sbt" % "process" % sbtVersion
+  
+  val akkaActor            = "com.typesafe.akka" % "akka-actor" % "2.0.3"
+  
+  val webjarsPlay          = "org.webjars" % "webjars-play" % "2.0"
+  val webjarsBootstrap     = "org.webjars" % "bootstrap" % "2.2.1"
+  val commonsIo            = "commons-io" % "commons-io" % "2.0.1"
+  val junitInterface       = "com.novocode" % "junit-interface" % "0.7"
 
   // Mini DSL
   // DSL for adding remote deps like local deps.
