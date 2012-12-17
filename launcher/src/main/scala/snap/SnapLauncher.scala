6574802897af95df7ea4d78f814191641be91c6d
Josh Suereth <joshua.suereth@gmail.com>
2012-12-17 12:52:05 -0500

    Add basic detection of snap projects.
    * If we find project/build.properties, we assume a snap friendly project.
    * If the user passes the shell argument, we just boot into the SBT shell.
    * If we aren't in UI mode, we don't have arguments and we're not in a project
      directory, then we display a help message, before exiting.

--- launcher/src/main/scala/snap/SnapLauncher.scala
+++ launcher/src/main/scala/snap/SnapLauncher.scala
@@ -6,19 +6,43 @@ import xsbti.{AppMain, AppConfiguration}
 class SnapLauncher extends AppMain {
 
   def run(configuration: AppConfiguration) =
-    // TODO - Detect non-interactive terminal
+    // TODO - Detect if we're running against a local project.
     try configuration.arguments match {
-      case Array("ui") => RebootToUI(configuration)
-      case _           => RebootToSbt(configuration)
+      case Array("ui")         => RebootToUI(configuration)
+      case Array("shell")      => RebootToSbt(configuration, useArguments=false)
+      case _ if isLocalProject => RebootToSbt(configuration)
+      case _                   => displayHelp(configuration)
     } catch {
-      case e: Exception =>
-        // TODO - Generate some kind of error report here.
-        Exit(1)
+      case e: Exception        => generateErrorReport(e)
     }
   // Wrapper to return exit codes.
   case class Exit(val code: Int) extends xsbti.Exit
-}
 
+  def displayHelp(configuration: AppConfiguration) = {
+    System.err.println("""| Warning:  Could not detect a local snap project.
+                          |
+                          | If you'd like to run snap in this directory, please:
+                          |
+                          | 1. Run the UI with `snap ui`
+                          | 2. Create a project with `snap create`
+                          | 3. Move into a snap project directory and re-run snap.
+                          |""".stripMargin)
+    Exit(1)
+  }
+
+  def generateErrorReport(e: Exception) = {
+    // TODO - Make a real error report.
+    e.printStackTrace()
+    Exit(2)
+  }
+
+  // TODO - Add better local project detection.
+  def isLocalProject: Boolean = {
+    // We assume we test against CWD here...
+    val file = new java.io.File("project/build.properties")
+    file.exists
+  }
+}
 // Wrapper to return the UI application.
 // TODO - Generate this via SBT code, so the hard-coded settings come
 // from the build.
@@ -37,8 +61,8 @@ case class RebootToUI(configuration: AppConfiguration) extends xsbti.Reboot {
 // TODO - Generate this via the SBT build code, so the hardcoded SBT version
 // lives in one spot.
 // OR we can even detect the SBT version...
-case class RebootToSbt(configuration: AppConfiguration) extends xsbti.Reboot {
-  val arguments = configuration.arguments
+case class RebootToSbt(configuration: AppConfiguration, useArguments: Boolean = false) extends xsbti.Reboot {
+  val arguments = if(useArguments) configuration.arguments else Array.empty[String]
   val baseDirectory = configuration.baseDirectory
   val scalaVersion = "2.9.2"
   val app = ApplicationID(
