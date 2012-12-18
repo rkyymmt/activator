85ae20e9bd991a34472294688d118b27349a642b
Josh Suereth <joshua.suereth@gmail.com>
2012-12-18 14:13:11 -0500

    Updating the SnapProperties file to have hierarchical properties.
    * Expanded SnapProperties to include more locations, such as the template cache, the template repository, the
      snap launcher files, etc.
    * Added hierarchical lookup model to SnapProperties so you can override the defaults using java properties
      or system environment variables.   When running play locally, going to try to have it run directly
      against our local project (without forcing a stage).  If that's not possible, we'll look at other means
      of running correctly.
    
    This is phase two of trying to push runtime lookup of snap stuff into one place, so it's easy to
    modify in the future.

--- props/src/main/java/snap/properties/SnapProperties.java
+++ props/src/main/java/snap/properties/SnapProperties.java
@@ -16,12 +16,81 @@ public class SnapProperties {
   }
 
   private static Properties props = loadProperties();
+  /** Checks the system properties, before the environment, before the hard coded defaults. */
+  private static String getProperty(String name) {
+    String value = System.getProperty(name);
+    if(value == null) {
+      value = System.getenv(name);
+    }
+    if(value == null) {
+      value = props.getProperty(name);
+    }
+    return value; 
+  }
+  
+  /** Looks up a property value, and parses its value as appropriate. */
+  private static String lookupOr(String name, String defaultValue) {
+    String value = getProperty(name);
+    if(value == null) {
+      value = defaultValue;
+    }
+    return value;
+  }
 
   public static String APP_VERSION() {
     return props.getProperty("app.version");
   }
 
+  public static String APP_ABI_VERSION() {
+    // TODO - Encode ABI version in SNAP metadata...
+    return APP_VERSION();
+  }
+
   public static String SBT_VERSION() {
     return props.getProperty("sbt.version");
+  }
+
+  public static String SNAP_HOME() {
+    return getProperty("snap.home");
+  }
+
+  public static String SNAP_USER_HOME() {
+    return lookupOr("snap.user.home", getProperty("user.home") + "/.snap/" + APP_ABI_VERSION());
+  }
+
+  public static String SNAP_TEMPLATE_CACHE() {
+    return lookupOr("snap.template.cache", SNAP_USER_HOME() + "/templates");
+  }
+
+  public static String SNAP_TEMPLATE_LOCAL_REPO() {
+    String defaultValue = SNAP_HOME();
+    if(defaultValue != null) {
+      defaultValue = defaultValue + "/templates";
+    }
+    return lookupOr("snap.template.localrepo", defaultValue);
+  }
+
+  public static String SNAP_LAUNCHER_JAR() {
+    String value = SNAP_HOME();
+    String version = APP_VERSION();
+    if(value != null && version != null) {
+      value = value + "/snap-launcher-"+version+".jar";
+    }
+    return value;
+  }
+
+  public static String SNAP_LAUNCHER_BAT() {
+    String value = SNAP_HOME();
+    if(value != null) {
+      value = value + "/snap.bat";
+    }
+    return value;
+  }
+  public static String SNAP_LAUNCHER_BASH() {
+    String value = SNAP_HOME();
+    if(value != null) {
+      value = value + "/snap";
+    }
+    return value;
   }
 }
