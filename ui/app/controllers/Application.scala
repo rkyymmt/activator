e61435f42dff5afb3e4a81afc949f2e35657b7e2
Josh Suereth <joshua.suereth@gmail.com>
2012-12-20 11:46:33 -0500

    Moved Cache related actions into cache project.
    * Added new Templates controler that pushes template information to the UI when requested.
    * Added cache Actions class to store actions that the UI needs from caches, so we can re-use from command line.
    * Migrated the cache-related stuff out of the Application controller into the Templates controller.

--- ui/app/controllers/Application.scala
+++ ui/app/controllers/Application.scala
@@ -9,35 +9,6 @@ import java.io.File
 import org.apache.commons.io.FileUtils
 
 object Application extends Controller {
-  // This will load our template cache and ensure all templates are available for the demo.
-  // We should think of an alternative means of loading this in the future.
-  val templateCache = snap.cache.TemplateCache()
-
-  def cloneTemplate = Action(parse.json) { request =>
-    
-    val name = (request.body \ "name").as[String]
-    val location = new File((request.body \ "location").as[String])
-    val templateid = (request.body \ "template").as[String]
-    
-    val template = templateCache.template(templateid)
-    if (!template.isDefined) {
-      NotAcceptable("Template " + templateid + " not found") // todo: to json
-    }
-    else if (location.exists()) {
-      NotAcceptable("Location " + location.getAbsolutePath + " exists") // todo: to json
-    }
-    else {
-      //  Copy all files from the template dir.
-      // TODO - Use SBT IO when it's available.
-      for {
-        t <- template
-        (file, path) <- t.files
-        to = new File(location, path)
-      } if(file.isDirectory) snap.cache.IO.createDirectory(to)
-        else snap.cache.IO.copyFile(file, to)
-      Ok(request.body)
-    }
-  }
   
   def openLocation = Action(parse.json) { request =>
 
