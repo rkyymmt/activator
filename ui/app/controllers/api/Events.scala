ce6114db053d563b5ad23f3b43a0841138105949
Josh Suereth <joshua.suereth@gmail.com>
2013-01-06 10:54:12 -0500

    Attempting to do better cleanup/garbage collection of event streams.  I think we've uncovered a solid bug here.  Needs more investigation to find out, but SSE channels aren't ever cleaned/closed from the client side, leaving leaked actors on the server side.

--- ui/app/controllers/api/Events.scala
+++ ui/app/controllers/api/Events.scala
@@ -10,6 +10,8 @@ import play.api.libs.iteratee.{ Enumerator, Enumeratee }
 import akka.actor.{Actor,ActorRef, Props}
 import play.api.libs.iteratee.Concurrent
 import play.api.libs.iteratee.Concurrent.Channel
+import concurrent.Future
+import akka.pattern.ask
 
 /** This controller handles piping events out to folks. */
 object Events extends Controller {
@@ -19,14 +21,11 @@ object Events extends Controller {
           CONTENT_TYPE -> "text/event-stream"
       )) 
   // Enumerator that starts a new actor listening to the event stream per-event-stream request.
-  private val events: Enumerator[String] = Concurrent.unicast(
-      onStart = { channel =>
-        println("Making downstream event handler!")
-        snap.Akka.events ! Props(new EventStreamActor(channel))
-      }, 
-      onComplete = () => println("Events enumerator stopped!"), 
-      onError = (msg, input) => println("Events enumerator errors!: " + msg))
-      
+  // What's annoying here is we have to *manually* create a context that is closed over by three closures
+  // and used rather than just extend a class.
+  private val events: Enumerator[String] = EventStreamEnumerator()
+  
+  // TODO - We should probably take a SSEvent() class here, so we can support all the spec, including ids and retry and such.
   private val toEventSource = Enumeratee.map[String](msg => s"data: ${msg}\n\n\n")
   // Action that returns a new event source stream.    
   def eventSource = Action {
@@ -40,14 +39,53 @@ object Events extends Controller {
     snap.Akka.events ! """{ "type": "test", "msg": "TEST EVENT" }"""
     Ok("OK")
   }
-  
-  // Simple actor that just pushes messages to teh pushee for an enumerator.
-  class EventStreamActor(channel: Channel[String]) extends Actor {
-    // TODO - Close the channel sometime?
-    def receive: Receive = {
-      case msg: String     => 
-        println("DOWNSTREAM EVENT HANDLER GOT: "+msg)
-        channel push msg
+
+  def kill = Action {
+    snap.Akka.events ! "Kill Children"
+    Ok("OK")
+  }
+
+
+}
+
+// This object constructs a new Enumerator that pushes the event JSON strings it receives through
+// play's iteratee framework.
+object EventStreamEnumerator {
+  // Here we create an enumerator where we have a context closed over by all three closures so that
+  // we can reference our start from all three closures.
+  // Most importantly, we attempt to cleanup our garbage.
+  // However, onComplete and onError are *not* being called
+  // when a connection is closed.   have to look into that.
+  def apply(): Enumerator[String] = {
+    val context = new EventActorContext(null)
+    Concurrent.unicast(
+      onStart = { channel =>
+        println("Making downstream event handler!")
+        implicit val t: akka.util.Timeout = akka.util.Timeout(concurrent.duration.FiniteDuration(10, "s"))
+        context.actorListener = (snap.Akka.events ? Props(new EventStreamActor(channel))).mapTo[ActorRef]
+      }, 
+      onComplete = () => context.killListener(), 
+      onError = (msg, input) => context.killListener())
+  }
+
+
+  private class EventActorContext(var actorListener: Future[ActorRef]) {
+    // Called when the client closes an event stream, so we can clean up.
+    def killListener(): Unit = {
+      import snap.Akka.system.dispatcher
+      println("Stoping event channel - client no longer listening.")
+      actorListener map (_ ! akka.actor.PoisonPill)
     }
   }
+  // Simple actor that just pushes messages it receives into the message channel.
+  private class EventStreamActor(channel: Channel[String]) extends Actor {
+    // TODO - Close the channel sometime?
+    def receive: Receive = {
+      case msg: String => channel push msg
+    }
+    // Ensure we stop the channel, if we can.
+    // TODO - Ignore failure?
+    override def postStop():Unit = channel.end()
+  }
+
 }
--- ui/app/snap/Akka.scala
+++ ui/app/snap/Akka.scala
@@ -22,14 +22,25 @@ import akka.actor.{Actor, ActorRef, Props}
 
 // TODO - Cleanup and thread stuff.
 class EventActor extends Actor {
-  var listeners: Seq[ActorRef] = Nil
-  
+  import akka.actor.{OneForOneStrategy, SupervisorStrategy}
+  import SupervisorStrategy.Stop
+  import concurrent.duration.Duration.Zero
+  // When one of our children has an error, we just stop the stream for now and assume the client will reconnect and
+  // make a new listener.
+  override val supervisorStrategy = OneForOneStrategy(0, Zero) {
+     case _ => Stop
+  } 
+
   def receive: Receive = {
+    case "Kill Children" =>
+      context.children foreach context.stop
     case newListenerProps: Props =>
-      val newListener = context actorOf newListenerProps
-      listeners +:= newListener
+      // Make a new listener using the set of props and return the ActorRef.
+      // Note: We don't have to keep track of it, because our context monitors our children.
+      sender ! (context actorOf newListenerProps)
     case msg: String =>
-      println("MASTER EVENT HANDLER GOT: "  + msg)
-      listeners foreach (_ ! msg)
+      println(s"Sending msg to ${context.children.toIndexedSeq.size} children")
+      context.children foreach (_ ! msg)
+    // TODO - Take in event messages we can adapt into JSON strings...
   }
 }
