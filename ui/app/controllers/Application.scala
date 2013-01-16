93faff816841c11dd2887736258a69c77dc8fe92
Josh Suereth <joshua.suereth@gmail.com>
2013-01-16 17:25:28 -0500

    Initial cut at controller fed plugins for the UI.
    This brings with it the basic notion of having a Play controller generate the require.js javascript
    of which plugins to load with the application, requiring a real refresh if we every add a new plugin.
    We still need to work out actual plugin hosting and some kind of plugin manager that tells us which
    'entry point' to load for a given plugin, but we're approaching zen.

---
+++ ui/app/controllers/Application.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import java.io.File
+
+case class ApplicationModel(
+    location: String,
+    plugins: Seq[String])
+
+// Here is where we detect if we're running at a given project...
+object Application extends Controller {
+  def index = Action {
+    if(isOnProject(cwd)) Redirect(routes.Application.app)
+    else Ok("TODO - Please create a project. (" + cwd + ")")  // TODO - view to create template
+  }
+
+  def app = Action { request =>
+    val location = request.getQueryString("location") map (new File(_)) getOrElse cwd
+    if(isOnProject(location)) Ok(views.html.application(getApplicationModel(location)))
+    else Redirect(routes.Application.index)
+  }
+  
+  
+  // TODO - actually load from file or something which plugins we use.
+  def getApplicationModel(projectDir: File) =
+    ApplicationModel(projectDir.getAbsolutePath,
+        Seq("plugins/demo/demo"))
+  
+  
+  // TODO - Better detection, in library most likely.
+  val cwd = (new java.io.File(".").getAbsoluteFile.getParentFile)
+  def isOnProject(dir: File) = (new java.io.File(dir, "project/build.properties")).exists
+}
