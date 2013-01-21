679bd859fae11b4c284921243e5aa4d8515eccf3
Maxime DANTEC <hello@warry.fr>
2013-01-21 22:59:25 +0100

    remove useless files

--- ui/app/assets/plugins/todo/todo.js
+++
@@ -1,6 +0,0 @@
-define(['css!./todo','./todo'], {
-	name: "TODO",
-	render: function(){
-		return "<article><header><h1>Todo</h1></header><footer><p></p></footer><section class=\"wrapper\"></section></article>"
-	}
-})
