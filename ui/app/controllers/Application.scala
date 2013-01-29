3b1137effcae49c1a3cd4f3cbe0c8f5d50f2f10c
Josh Suereth <joshua.suereth@gmail.com>
2013-01-29 14:44:07 -0500

    App name now part of the URL.
    * /app/name now works (stubbed impl) instead of just /app
    * / now redirects to /app/name or / depending on what we know about sbt project.
    * Added stub implementations of certain methods for @havocp.

--- ui/app/controllers/Application.scala
+++ ui/app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api.mvc.{ Action, Controller }
 import java.io.File
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 case class ApplicationModel(
   location: String,
@@ -13,26 +15,59 @@ case class ApplicationModel(
 // Here is where we detect if we're running at a given project...
 object Application extends Controller {
   def index = Action {
-    if (isOnProject(cwd)) Redirect(routes.Application.app)
-    else Ok(views.html.home())
+    Async {
+      loadAppName(cwd) map {
+        // TODO - Wait for cached name....
+        case Some(name) => Redirect(routes.Application.app(name))
+        case _ => Redirect(routes.Application.forceHome)
+      }
+    }
   }
 
   def forceHome = Action { request =>
     Ok(views.html.home())
   }
 
-  def app = Action { request =>
-    val location = request.getQueryString("location") map (new File(_)) getOrElse cwd
-    if (isOnProject(location)) Ok(views.html.application(getApplicationModel(location)))
-    else Redirect(routes.Application.index)
+  def app(name: String) = Action { request =>
+    Async {
+      // TODO - Different results of attempting to load the application....
+      loadApp(name) map {
+        case Some(app) => Ok(views.html.application(getApplicationModel(app)))
+        case _ => Redirect(routes.Application.forceHome)
+      }
+    }
   }
 
   // TODO - actually load from file or something which plugins we use.
-  def getApplicationModel(projectDir: File) =
-    ApplicationModel(projectDir.getAbsolutePath,
+  def getApplicationModel(app: snap.App) =
+    ApplicationModel(app.config.location.getAbsolutePath,
       Seq("plugins/code/code", "plugins/play/play"))
 
   // TODO - Better detection, in library most likely.
   val cwd = (new java.io.File(".").getAbsoluteFile.getParentFile)
+
+  // Loads an application based on its name.
+  // TODO - Don't have this stubbed out!
+  private def loadApp(name: String): Future[Option[snap.App]] =
+    Future {
+      // TODO ->
+      Some(new snap.App(
+        snap.ProjectConfig(
+          cwd,
+          Some(name)),
+        snap.Akka.system,
+        null))
+    }
+  // Loads the name of an app based on the CWD.  Note: This is how we
+  // redirect when first starting the UI.   We need to detect if the location
+  // is an sbt project, start SBT and pull out the name of the root project,
+  // Then return that name (as well as storing it in the recent app cache...)
+  private def loadAppName(location: File): Future[Option[String]] =
+    Future {
+      // TODO - Don't cheat here!
+      if (isOnProject(location)) Some("my-app")
+      else None
+    }
+
   def isOnProject(dir: File) = (new java.io.File(dir, "project/build.properties")).exists
 }
