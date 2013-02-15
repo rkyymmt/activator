15e380138fbc6ac1d72968e3b2c58ae4ac08e7e0
Josh Suereth <joshua.suereth@gmail.com>
2013-02-15 11:06:50 -0500

    Fixing plugin API to load default 'ace' editor binding. * Adding hooks for highlight mode and theme. * Migrate ACE junk out of code plugin and into general purpose, re-usable area. * Remove ace import from application.scala.html. * Fix CSS so things render appropraitely again.

--- ui/app/assets/javascripts/core/pluginapi.js
+++ ui/app/assets/javascripts/core/pluginapi.js
@@ -1,6 +1,11 @@
-define(['vendors/knockout-2.2.1.debug', './sbt', './templates', 'vendors/keymage.min', './utils'], function(ko, sbt, templates, key, utils) {
-
-
+define([
+	'vendors/knockout-2.2.1.debug',
+	'./sbt',
+	'./templates',
+	'vendors/keymage.min',
+	'./utils',
+	'vendors/ace/ace'],
+	function(ko, sbt, templates, key, utils, ignore_ace) {
 //base class for widgets, with convenience.
 //All widget classes should support the following static fields:
 //  id - The identifier of the widget.
@@ -28,7 +33,68 @@ var Widget = function(o) {
 		onRender: function(elements) {}
 	});
 	return WidgetClass;
-}
+};
+
+// Add knockout bindings for ace editor.  Try to capture all info we need
+// here so we don't have to dig in like crazy when we need a good editor.
+// Example:
+//  <div class="editor" data-bind="ace: contents"/>
+// <div class="editor" data-bind="ace: { contents: contents, theme: 'ace/theme/xcode', dirty: isEditorDirty, highlight: 'scala' }"/>
+ko.bindingHandlers.ace = {
+		init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+			// First pull out all the options we may or may not use.
+			var options = valueAccessor();
+			// If they only provide a text field to bind into, we allow that too.
+			var editorValue = options.contents || options;
+			var dirtyValue = options.dirty;
+			// TODO - unwrap observable?
+			var theme = options.theme || 'ace/theme/xcode';
+			var highlight = options.highlight || 'scala';
+
+			// We have to write our text into the element before instantiating the editor.
+			$(element).text(ko.utils.unwrapObservable(editorValue))
+
+			var editor = ace.edit(element);
+			editor.setTheme(ko.utils.unwrapObservable(theme));
+			// TODO - Check for no highlight mode as well, or allow non-built-in
+			// highlighting...
+			editor.getSession().setMode('ace/mode/'+ko.utils.unwrapObservable(highlight));
+
+			// Assume we can sneak this on here.
+			viewModel.editor = editor;
+			//handle edits made in the editor
+			editor.getSession().on('change', function (e) {
+				if (ko.isWriteableObservable(editorValue)) {
+					editorValue(editor.getValue());
+				}
+				// mark things dirty after an edit.
+				if(ko.isWriteableObservable(dirtyValue)) {
+					dirtyValue(true);
+				}
+			});
+			// Ensure things are cleaned on destruction.
+			ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+				editor.destroy();
+			});
+		},
+		update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+			var options = valueAccessor();
+			var editorValue = options.contents;
+			var dirtyValue = options.dirty;
+			var content = ko.utils.unwrapObservable(editorValue);
+			var editor = viewModel.editor;
+			// TODO - Don't freaking do this all the time.  We should not
+			// involved in changes we caused.
+			if(editor.getValue() != content) {
+				editor.setValue(content, editor.getCursorPosition());
+				// Update dirty value.
+				if(ko.isWriteableObservable(dirtyValue)) {
+					dirtyValue(false);
+				}
+			}
+		}
+	};
+
 
 	return {
 		ko: ko,
--- ui/app/assets/plugins/code/codeView.js
+++ ui/app/assets/plugins/code/codeView.js
@@ -1,55 +1,5 @@
 define(["text!./viewCode.html", 'core/pluginapi'], function(template, api){
 	var ko = api.ko;
-	ko.bindingHandlers.ace = {
-		init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-			// First pull out all the options we may or may not use.
-			var options = valueAccessor();
-			// If they only provide a text field to bind into, we allow that too.
-			var editorValue = options.contents || options;
-			var dirtyValue = options.dirty;
-
-			// We have to write our text into the element before instantiating the editor.
-			$(element).text(ko.utils.unwrapObservable(editorValue))
-
-			var editor = ace.edit(element);
-			editor.setTheme("ace/theme/textmate");
-			//TODO - Figure out highlight mode!...
-			editor.getSession().setMode("ace/mode/scala");
-
-			// Assume we can sneak this on here.
-			viewModel.editor = editor;
-			//handle edits made in the editor
-			editor.getSession().on('change', function (e) {
-				if (ko.isWriteableObservable(editorValue)) {
-					editorValue(editor.getValue());
-				}
-				// mark things dirty after an edit.
-				if(ko.isWriteableObservable(dirtyValue)) {
-					dirtyValue(true);
-				}
-			});
-			// Ensure things are cleaned on destruction.
-			ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-				editor.destroy();
-			});
-		},
-		update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-			var options = valueAccessor();
-			var editorValue = options.contents;
-			var dirtyValue = options.dirty;
-			var content = ko.utils.unwrapObservable(editorValue);
-			var editor = viewModel.editor;
-			// TODO - Don't freaking do this all the time.  We should not
-			// involved in changes we caused.
-			if(editor.getValue() != content) {
-				editor.setValue(content, editor.getCursorPosition());
-				// Update dirty value.
-				if(ko.isWriteableObservable(dirtyValue)) {
-					dirtyValue(false);
-				}
-			}
-		}
-	};
 
 	// Fetch utility
 	function show(location){
