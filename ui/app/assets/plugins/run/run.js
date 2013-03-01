5ccc7dd4fcb6d8f778939a2b145f3b2f27c1747d
Havoc Pennington <hp@pobox.com>
2013-03-01 10:02:38 -0500

    Add Restart button to the Run plugin

--- ui/app/assets/plugins/run/run.js
+++ ui/app/assets/plugins/run/run.js
@@ -28,6 +28,7 @@ define(['text!./run.html', 'core/pluginapi'], function(template, api){
 				return self.activeTask() != "";
 			}, this);
 			this.rerunOnBuild = ko.observable(false);
+			this.restartPending = ko.observable(false);
 
 			api.events.subscribe(function(event) {
 				return event.type == 'CompileSucceeded';
@@ -73,16 +74,29 @@ define(['text!./run.html', 'core/pluginapi'], function(template, api){
 				self.doRun(true); // true=triggeredByBuild
 			}
 		},
+		doAfterRun: function() {
+			var self = this;
+			self.activeTask("");
+			if (self.restartPending()) {
+				self.doRun(false); // false=!triggeredByBuild
+			}
+		},
 		doRun: function(triggeredByBuild) {
 			var self = this;
 
 			self.logs.removeAll();
 			self.output.removeAll();
+
 			if (triggeredByBuild) {
 				self.logs.push("Build succeeded, running...");
+			} else if (self.restartPending()) {
+				self.logs.push("Restarting...");
 			} else {
 				self.logs.push("Running...");
 			}
+
+			self.restartPending(false);
+
 			var task = { task: 'RunRequest' };
 			if (self.haveMainClass())
 				task.params = { mainClass: self.currentMainClass() };
@@ -112,7 +126,6 @@ define(['text!./run.html', 'core/pluginapi'], function(template, api){
 				},
 				success: function(data) {
 					console.log("run result: ", data);
-					self.activeTask("");
 					if (data.type == 'ErrorResponse') {
 						self.logs.push(data.error);
 					} else if (data.type == 'RunResponse') {
@@ -120,20 +133,18 @@ define(['text!./run.html', 'core/pluginapi'], function(template, api){
 					} else {
 						self.logs.push('Unexpected reply: ' + JSON.stringify(data));
 					}
+					self.doAfterRun();
 				},
 				failure: function(xhr, status, message) {
 					console.log("run failed: ", status, message)
-					self.activeTask("");
 					self.logs.push("HTTP request failed: " + message);
+					self.doAfterRun();
 				}
 			});
 			self.activeTask(taskId);
 		},
-		startButtonClicked: function(self) {
-			console.log("Run was clicked");
-			self.doRun(false); // false=!triggeredByBuild
-		},
-		stopButtonClicked: function(self) {
+		doStop: function() {
+			var self = this;
 			if (self.haveActiveTask()) {
 				sbt.killTask({
 					taskId: self.activeTask(),
@@ -146,6 +157,20 @@ define(['text!./run.html', 'core/pluginapi'], function(template, api){
 					}
 				});
 			}
+		},
+		startButtonClicked: function(self) {
+			console.log("Run was clicked");
+			self.doRun(false); // false=!triggeredByBuild
+		},
+		restartButtonClicked: function(self) {
+			console.log("Restart was clicked");
+			self.doStop();
+			self.restartPending(true);
+		},
+		stopButtonClicked: function(self) {
+			console.log("Stop was clicked");
+			self.restartPending(false);
+			self.doStop();
 		}
 	});
 
