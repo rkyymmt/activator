c45e61f586fa5c35a76edca60b95802e25cf1d67
Josh Suereth <joshua.suereth@gmail.com>
2013-03-12 10:43:14 -0400

    Bump the template CLI to use autocompletion.  We don't support all use cases, but we work now.

--- launcher/src/main/scala/snap/SnapCli.scala
+++ launcher/src/main/scala/snap/SnapCli.scala
@@ -4,9 +4,10 @@ import xsbti.{ AppMain, AppConfiguration }
 import snap.cache.TemplateCache
 import snap.cache.Actions.cloneTemplate
 import java.io.File
+import sbt.complete.{ Parser, Parsers }
 
 object SnapCli {
-  def apply(configuration: AppConfiguration): Int = {
+  def apply(configuration: AppConfiguration): Int = try {
     System.out.println()
     val name = getApplicationName()
     val projectDir = new File(name).getAbsoluteFile
@@ -30,9 +31,12 @@ object SnapCli {
         printUsage(name, projectDir)
         0
       case _ =>
-        System.err.println("Could not find template with name: $templateName")
-        1
+        sys.error("Could not find template with name: $templateName")
     }
+  } catch {
+    case e: Exception =>
+      System.err.println(e.getMessage)
+      1
   }
 
   private def printUsage(name: String, dir: File): Unit = {
@@ -51,21 +55,36 @@ object SnapCli {
 
   private def getApplicationName(): String = {
     System.out.println("Enter an application name")
-    System.out.print("> ")
-    readLine()
+    val appNameParser: Parser[String] = {
+      import Parser._
+      import Parsers._
+      token(any.* map { _ mkString "" }, "<application name>")
+    }
+
+    readLine(appNameParser) filterNot (_.isEmpty) getOrElse sys.error("No application name specified.")
   }
 
-  private def readLine(): String = {
-    val in = new java.io.BufferedReader(new java.io.InputStreamReader(System.in))
-    in.readLine()
-  }
-
-  private def bufferString(length: Int)(in: String): String =
-    in + (0 to (length - in.length)).map(_ => ' ').mkString("")
   private def getTemplateName(possible: Seq[String]): String = {
+    val templateNameParser: Parser[String] = {
+      import Parser._
+      import Parsers._
+      token(any.* map { _ mkString "" }, "<template name>").examples(possible.toSet, false)
+    }
+
     System.out.println("Enter a blueprint name, or hit press tab to see the list")
     // TODO - Autocomplete on tab!
-    System.out.print("> ")
-    readLine()
+    readLine(templateNameParser) filterNot (_.isEmpty) getOrElse sys.error("No template name specified.")
+  }
+
+  /** Uses SBT complete library to read user input with a given auto-completing parser. */
+  private def readLine[U](parser: Parser[U], prompt: String = "> ", mask: Option[Char] = None): Option[U] = {
+    val reader = new sbt.FullReader(None, parser)
+    reader.readLine(prompt, mask) flatMap { line =>
+      val parsed = Parser.parse(line, parser)
+      parsed match {
+        case Right(value) => Some(value)
+        case Left(e) => None
+      }
+    }
   }
 }
