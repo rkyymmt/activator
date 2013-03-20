6faddeb1054f8b1a893b0b06ff8681143cca27a0
Josh Suereth <joshua.suereth@gmail.com>
2013-03-20 12:42:02 -0400

    Adding detection of previously running Builder.
    * Use a lockfile in the builder home directory to prevent too many people touchign the PID
    * Fail-fast => If we can't detect processes or PIDs, just try to start up, rather than
      prevent the user from any working behavior.
    * Attempts to detect PIDs on windows + linux (Still needs to be tested)

--- ui/app/builder/UIMain.scala
+++ ui/app/builder/UIMain.scala
@@ -8,29 +8,111 @@ import java.net.HttpURLConnection
 import java.net.URL
 import com.typesafe.sbtchild.SbtChildLauncher
 import snap._
+import builder.properties.BuilderProperties._
+import xsbti.GlobalLock
+
+object PidDetector {
+  // Converts to callable
+  private[PidDetector] def f2c[A](f: => A): java.util.concurrent.Callable[A] =
+    new java.util.concurrent.Callable[A] {
+      def call: A = f
+    }
+  // Helper to make global lock easier to use.
+  implicit class LockHelper(val lock: GlobalLock) extends AnyVal {
+    def onBuilder[A](f: => A): A =
+      lock.apply(BUILDER_LOCK_FILE, f2c(f))
+  }
+  // Checks to see if we can load the previous pid.
+  private def previousPid(lock: GlobalLock): Option[String] =
+    lock onBuilder {
+      if (BUILDER_PID_FILE.exists)
+        Option(snap.cache.IO.slurp(BUILDER_PID_FILE).trim) filterNot (_.isEmpty)
+      else None
+    }
+
+  private def writeCurrentPid(lock: GlobalLock): Unit = {
+    for (pid <- java.lang.management.ManagementFactory.getRuntimeMXBean.getName.split('@').headOption) {
+      lock onBuilder {
+        System.out.println("Updating pid file to: " + pid)
+        snap.cache.IO.write(BUILDER_PID_FILE, pid, append = false)
+      }
+    }
+  }
+  import sys.process._
+  private def psListHasPid(pid: String, pb: ProcessBuilder): Boolean =
+    try {
+      for {
+        line <- pb.lines
+        p <- line.split("\\s+").filterNot(_.isEmpty).headOption
+        if p contains pid
+      } return true
+      false
+    } catch {
+      // TODO - What kind of process failure exceptions do we have to ignore?
+      case e: Exception => // Ignore
+        false
+    }
+  private def findProcessLinux(pid: String): Boolean =
+    psListHasPid(pid, Seq("ps", "-e"))
+  private def findProcessWindows(pid: String): Boolean =
+    psListHasPid(pid, Seq("cmd", "/c", "jps"))
+
+  // Process detection on both windows and linux...
+  // These default to returning false if they can't detect proceses at all.
+  private def findProcess(pid: String): Boolean =
+    if (sys.props("os.name").toLowerCase contains "windows") findProcessWindows(pid)
+    else findProcessLinux(pid)
+
+  /**
+   * Checks to see if there is a previous running application.
+   * If so, we return true, otherwise we return false.
+   *
+   * SIDE EFFECT WARNING - This updates the PID file and does file locking.
+   */
+  def checkRunning(lock: GlobalLock): Boolean = {
+    val checkPrevious =
+      (for {
+        pid <- previousPid(lock)
+        _ = println("Found previous process id: " + pid)
+        if findProcess(pid)
+      } yield true) getOrElse false
+
+    if (!checkPrevious) writeCurrentPid(lock)
+    checkPrevious
+  }
+}
 
 /** Expose for SBT launcher support. */
 class UIMain extends AppMain {
 
   def run(configuration: AppConfiguration) = {
+
+    val lock = configuration.provider.scalaProvider.launcher.globalLock
+    // First we check to see if a previous app is running..
+    val alreadyRunning = PidDetector.checkRunning(lock)
+
     // First set up the server port.
     System.setProperty("http.port", serverPort.toString)
 
-    // locate sbt details and store in a singleton
-    snap.AppManager.sbtChildProcessMaker = new SbtChildLauncher(configuration)
+    if (!alreadyRunning) {
+      // locate sbt details and store in a singleton
+      snap.AppManager.sbtChildProcessMaker = new SbtChildLauncher(configuration)
 
-    // Start the Play app... (TODO - how do we know when we're done?)
-    // TODO - Is this hack ok?
-    withContextClassloader(play.core.server.NettyServer.main(configuration.arguments))
+      // Start the Play app... (TODO - how do we know when we're done?)
+      // TODO - Is this hack ok?
+      withContextClassloader(play.core.server.NettyServer.main(configuration.arguments))
 
-    // Delay opening the browser a short bit so play can start.
-    waitForServerStartup()
+      // Delay opening the browser a short bit so play can start.
+      waitForServerStartup()
+    } else {
+      System.err.println("Connecting to existing builder UI server...")
+    }
 
     // openBrowser
     openBrowser()
 
     // Prevent us from dropping to exit immediately... block on Play running until you see CTRL-C or some such....
-    waitForever()
+    if (!alreadyRunning) waitForever()
     // TODO - Catch errors and better return value.
     Exit(0)
   }
