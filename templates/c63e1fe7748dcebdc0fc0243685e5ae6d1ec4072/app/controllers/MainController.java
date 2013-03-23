aa1ec019cfa5db53adbb4fcd8647b1725eeb92b6
James Ward <james@jamesward.org>
2013-03-23 13:19:34 -0600

    avoid method name confusion

--- templates/c63e1fe7748dcebdc0fc0243685e5ae6d1ec4072/app/controllers/MainController.java
+++ templates/c63e1fe7748dcebdc0fc0243685e5ae6d1ec4072/app/controllers/MainController.java
@@ -2,12 +2,11 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
 
 public class MainController extends Controller {
     
     public static Result index() {
-        return ok(index.render("Hello from Java"));
+        return ok(views.html.index.render("Hello from Java"));
     }
     
 }
