c1b680a2bedd2f49e01e0d90c9473c27b55f8e52
Josh Suereth <joshua.suereth@gmail.com>
2013-04-12 09:00:12 -0400

    Fixes to detect javac and JDK.

--- dist/src/templates/builder.bat
+++ dist/src/templates/builder.bat
@@ -44,18 +44,38 @@ rem Detect if this java is ok to use.
 for /F %%j in ('"%_JAVACMD%" -version  2^>^&1') do (
   if %%~j==Java set JAVAINSTALLED=1
 )
-if not defined JAVAINSTALLED (
+
+rem Detect the same thing about javac
+if "%_JAVACCMD%"=="" (
+  if not "%JAVA_HOME%"=="" (
+    if exist "%JAVA_HOME%\bin\javac.exe" set "_JAVACCMD=%JAVA_HOME%\bin\javac.exe"
+  )
+)
+if "%_JAVACCMD%"=="" set _JAVACCMD=javac
+for /F %%j in ('"%_JAVACCMD%" -version 2^>^&1') do (
+  if %%~j==javac set JAVACINSTALLED=1
+)
+
+rem BAT has no logiclal or, so we do it OLD SCHOOL! Oppan Redmond Style
+set JAVAOK=true
+if not defined JAVAINSTALLED set JAVAOK=false
+if not defined JAVACINSTALLED set JAVAOK=false
+
+if "%JAVAOK%"=="false" (
   echo.
-  echo A Java JDK is not installed or can't be found at: 
-  echo %_JAVACMD%
+  echo A Java JDK is not installed or can't be found.
+  if not "%JAVA_HOME%"=="" (
+    echo JAVA_HOME = "%JAVA_HOME%"
+  )
   echo.
-  echo Please go to http://www.java.com/getjava/ and download
-  echo a valid Java JDK and install before running builder.
-  echo
+  echo Please go to
+  echo   http://www.oracle.com/technetwork/java/javase/downloads/index.html
+  echo and download a valid Java JDK and install before running builder.
+  echo.
   echo If you think this message is in error, please check
-  echo your environment variables to see if "java.exe" is
+  echo your environment variables to see if "java.exe" and "javac.exe" are
   echo available via JAVA_HOME or PATH.
-  echo
+  echo.
   if defined DOUBLECLICKED pause
   exit /B 1
 )
