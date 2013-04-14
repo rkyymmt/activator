0995f5034a34a1612ea2151d7c8b168eec63da3e
Jonas Bonér <jonas@jonasboner.com>
2013-04-13 21:25:17 -0700

    Rewrote the HelloAkka samples and tests for both Scala and Java.

--- templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaJava.java
+++ templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaJava.java
@@ -2,27 +2,63 @@ import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.util.Timeout;
+import akka.japi.Procedure;
+import akka.dispatch.*;
+import akka.pattern.Patterns;
+
+import scala.concurrent.Future;
+import scala.concurrent.ExecutionContext;
 import scala.concurrent.duration.Duration;
+
 import static java.util.concurrent.TimeUnit.*;
 
 public class HelloAkkaJava {
-
-    public static void main(String[] args) {
-        final ActorSystem system = ActorSystem.create("helloakka");
-        system.scheduler().schedule(Duration.Zero(),
-            Duration.create(5, SECONDS),
-            system.actorOf(new Props(HelloActorJava.class)),
-            "sayhello",
-            system.dispatcher()
-        );
+    public static class Greet {}
+    public static class WhoToGreet {
+        public final String who;
+        public WhoToGreet(String who) {
+            this.who = who;
+        }
     }
-
-    public static class HelloActorJava extends UntypedActor {
-
-        public void onReceive(Object message) {
-            if (message.equals("sayhello"))
-                System.out.println("hello, world");
+    public static class Greeting {
+        public final String message;
+        public Greeting(String message) {
+            this.message = message;
         }
     }
 
+    public static class Greeter extends UntypedActor {
+        String greeting = "";
+
+        public void onReceive(Object message) {
+            if (message instanceof WhoToGreet) {
+                greeting = "hello, " + ((WhoToGreet)message).who;
+                System.out.println("Changed greeting to " + greeting);
+            } else if (message instanceof Greet) {
+                getSender().tell(new Greeting(greeting));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("helloakka");
+        final ActorRef greeter = system.actorOf(new Props(Greeter.class), "greeter");
+
+        final ExecutionContext ec = system.dispatcher();
+        final Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+
+        // tell the greeter to change its greeting
+        greeter.tell(new WhoToGreet("akka"));
+
+        // ask the greeter for its current greeting
+        final Future<Object> f = Patterns.ask(greeter, new Greet(), timeout);
+        f.onSuccess(new OnSuccess<Object>() {
+          public void onSuccess(Object o) {
+            System.out.println(o);
+          }
+        }, ec);
+
+        system.shutdown();
+    }
 }
--- templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaScala.scala
+++ templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaScala.scala
@@ -1,22 +1,42 @@
-import java.util.concurrent.TimeUnit.SECONDS
-import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext.Implicits.global
 import akka.actor.{ActorSystem, Props, Actor}
+import akka.util.Timeout
+import akka.pattern.ask
+import scala.concurrent.duration._
 
-case object SayHello
+case object Greet
+case class WhoToGreet(who: String)
+case class Greeting(message: String)
 
-class HelloActorScala extends Actor {
+class Greeter extends Actor {
+  var greeting = ""
+
   def receive = {
-    case SayHello => println("hello, world")
+    case WhoToGreet(who) =>
+      greeting = s"hello, $who"
+      println(s"Changed greeting to $greeting")
+
+    case Greet =>
+      sender ! Greeting(greeting)
   }
 }
 
 object HelloAkkaScala extends App {
+
   val system = ActorSystem("helloakka")
-  
-  system.scheduler.schedule(Duration.Zero,
-    Duration.create(5, SECONDS),
-    system.actorOf(Props[HelloActorScala]),
-    SayHello)
-  
+
+  import system.dispatcher
+  implicit val timeout = Timeout(5 seconds)
+
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  // tell the greeter to change its greeting
+  greeter tell WhoToGreet("akka")
+
+  // ask the greeter for its current greeting
+  for (Greeting(message) <- greeter ask Greet) println(s"Greeting: $message")
+
+  greeter ! WhoToGreet("typesafe")
+  for (Greeting(message) <- greeter ask Greet) println(s"Greeting: $message")
+
+  system.shutdown()
 }
