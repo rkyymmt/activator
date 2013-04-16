3847c57c3f3d336a55566e81355dca42efbd5a79
James Ward <james@jamesward.org>
2013-04-16 14:52:57 -0600

    real-time charts are in

--- templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/project/Build.scala
+++ templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/project/Build.scala
@@ -9,12 +9,12 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     "org.webjars" % "webjars-play" % "2.1.0-1",
-    "org.webjars" % "bootstrap" % "2.1.1"
+    "org.webjars" % "bootstrap" % "2.1.1",
+    "org.webjars" % "flot" % "0.8.0"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
   )
 
 }
