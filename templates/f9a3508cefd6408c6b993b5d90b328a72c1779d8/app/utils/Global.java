11363d9f2db9e248af21e37428bc3630ee54f1b3
James Ward <james@jamesward.org>
2013-04-17 11:12:45 -0600

    add sentiment service

--- templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/app/utils/Global.java
+++ templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/app/utils/Global.java
@@ -1,23 +1,33 @@
+package utils;
+
 import actors.FetchLatest;
-import actors.SetupStock;
 import actors.StockHolderActor;
 import actors.UsersActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import play.Application;
 import play.GlobalSettings;
+import play.Play;
 import play.libs.Akka;
 import scala.concurrent.duration.Duration;
 
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Global extends GlobalSettings {
 
+    public static String sentimentUrl;
+    public static String tweetUrl;
+    
     @Override
     public void onStart(Application application) {
 
+        tweetUrl = Play.application().configuration().getString("tweet.url");
+        sentimentUrl = Play.application().configuration().getString("sentiment.url");
+        
+        if ((sentimentUrl == null) || (tweetUrl == null)) {
+            throw new RuntimeException("Both sentiment.url and tweet.url configs must be specified");
+        }
+        
         ActorRef usersActor = Akka.system().actorOf(new Props(UsersActor.class), "users");
         
         ActorRef stockHolderActor = Akka.system().actorOf(new Props(StockHolderActor.class), "stocks");
