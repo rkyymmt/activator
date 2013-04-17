26709fee5bb413d93cad47a558acbbd8621733b1
Havoc Pennington <hp@pobox.com>
2013-04-17 15:39:37 -0400

    remove js util inUrl, doesn't appear to be used

--- ui/app/assets/javascripts/core/utils.js
+++ ui/app/assets/javascripts/core/utils.js
@@ -4,24 +4,6 @@ $.fn.getOrElse = function(selector, ctx) {
 	return this.length ? $(this) : $(selector, ctx)
 }
 
-function inUrl(path, url) {
-	url = url || window.location.hash;
-	url = url.search("#") < 0 ? url.substr(1) : url;
-	url = url.search("/") < 0 ? [url] : url.split('/');
-
-	path = path.search("#") < 0 ? path.substr(1) : path;
-	path = path.search("/") < 0 ? [path] : path.split('/');
-
-	if(url.length < path.length) return false;
-	else if(url.length > path.length) url.splice(path.length, -1);
-
-	for(i in url) {
-		if(url[i] != path[i]) return false;
-	}
-
-	return true;
-};
-
 var Class = function(o) {
 	function M() {
 		if(this.init) {
@@ -43,7 +25,6 @@ var Class = function(o) {
 
 
 	return {
-		Class: Class,
-		inUrl: inUrl
+		Class: Class
 	}
 });
