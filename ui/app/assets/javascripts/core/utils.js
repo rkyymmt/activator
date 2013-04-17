3650e9ba493cba363d182c11069c99546568eca0
Havoc Pennington <hp@pobox.com>
2013-04-17 15:38:47 -0400

    remove js util Event, doesn't appear to be used

--- ui/app/assets/javascripts/core/utils.js
+++ ui/app/assets/javascripts/core/utils.js
@@ -22,39 +22,6 @@ function inUrl(path, url) {
 	return true;
 };
 
-function noop() {};
-
-/* Dead simple events manager
- * Usage:
- * var evented = Event({
- * 	prop: ""
- * })
- * evented.bind("evt", function(e){
- * 	console.log(this, e)
- * })
- * evented.trigger("evt", "param")
- */
-var Event = function(o) {
-	o.bind = function(event, fct) {
-		this._events = this._events || {};
-		this._events[event] = this._events[event] || [];
-		this._events[event].push(fct);
-	}
-	o.unbind = function(event, fct) {
-		this._events = this._events || {};
-		if(event in this._events === false) return;
-		this._events[event].splice(this._events[event].indexOf(fct), 1);
-	}
-	o.trigger = function(event /* , args... */ ) {
-		this._events = this._events || {};
-		if(event in this._events === false) return;
-		for(var i = 0; i < this._events[event].length; i++) {
-			this._events[event][i].apply(o, Array.prototype.slice.call(arguments, 1));
-		}
-	}
-	return o;
-};
-
 var Class = function(o) {
 	function M() {
 		if(this.init) {
@@ -77,7 +44,6 @@ var Class = function(o) {
 
 	return {
 		Class: Class,
-		Event: Event,
 		inUrl: inUrl
 	}
 });
