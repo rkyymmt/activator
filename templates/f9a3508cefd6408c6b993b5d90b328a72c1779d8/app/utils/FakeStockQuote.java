8f0b9e52c8db57f5bc8360b79ad8dba6d0bcc6f8
James Ward <james@jamesward.org>
2013-04-16 18:45:28 -0600

    flip effect

--- templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/app/utils/FakeStockQuote.java
+++ templates/f9a3508cefd6408c6b993b5d90b328a72c1779d8/app/utils/FakeStockQuote.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class FakeStockQuote {
 
     public static Double newPrice(Double lastPrice) {
+        // todo: over time these values trend lower
         Double randomDeltaPecent = ((5 - (10 * new Random().nextDouble()) ) / 100); // +/- 5%
         Double newPrice = lastPrice * (1 + randomDeltaPecent);
         return newPrice;
