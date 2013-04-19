8be62e8aca949bce8bea0687bb0125f1636f2da9
Josh Suereth <joshua.suereth@gmail.com>
2013-04-19 09:43:52 -0400

    Tempalte metadata improvements.
    * Adds "title" to template metadata.
    * "versoin" becomes "timeStamp" given by Typesafe webserver.
    * "name" become URL/console friendly option.
    * The first tag is now displayed as a category for the template on the home page.

--- cache/src/main/scala/snap/cache/TemplateCache.scala
+++ cache/src/main/scala/snap/cache/TemplateCache.scala
@@ -7,9 +7,11 @@ import builder.properties.BuilderProperties
 
 // TODO - This may need more work.
 // TODO - We probably are on the limit of fields for a useful case-class....
-case class TemplateMetadata(id: String,
-  name: String,
-  version: String,
+case class TemplateMetadata(
+  id: String,
+  name: String, // Web/Console friendly-name  Must be unique.
+  title: String, // Human readable name for website
+  timeStamp: Long, // A timestamp of when this guy was generated
   description: String,
   tags: Seq[String]) {
   // TODO - update equality/hashcode to be based on ID
@@ -65,22 +67,25 @@ class DemoTemplateCache() extends TemplateCache {
       // TODO - Put more hardcoded template metadata for the demo here!
       TemplateMetadata(
         id = "a5227c77d39109b6550a47758c2f9a1341e06524",
-        name = "Hello Scala!",
-        version = "1.0",
+        name = "hello-scala",
+        title = "Hello Scala!",
+        timeStamp = 1,
         description = """Scala is a general purpose programming language designed to express common programming patterns in a concise, elegant, and type-safe way.  This very simple Scala application will get you started building and testing standalone Scala apps.  This app uses Scala 2.10 and ScalaTest.""",
-        tags = Seq("scala", "starter")),
+        tags = Seq("Introduction", "scala", "starter")),
       TemplateMetadata(
         id = "39836f5aa646b3a37abb80e8a2c335ebf6830cac",
-        name = "Hello Akka!",
-        version = "1.0",
+        name = "hello-akka",
+        title = "Hello Akka!",
+        timeStamp = 1,
         description = """Akka is a toolkit and runtime for building highly concurrent, distributed, and fault tolerant event-driven apps.  This simple application will get you started building Actor based systems in Java and Scala.  This app uses Akka 2.1, Java 6, Scala 2.10, JUnit, and ScalaTest.""",
-        tags = Seq("akka", "java", "scala", "starter")),
+        tags = Seq("Introduction", "akka", "java", "scala", "starter")),
       TemplateMetadata(
         id = "c63e1fe7748dcebdc0fc0243685e5ae6d1ec4072",
-        name = "Hello Play Framework!",
-        version = "1.0",
+        name = "hello-play",
+        title = "Hello Play Framework!",
+        timeStamp = 1,
         description = """Play Framework is the High Velocity Web Framework for Java and Scala.  Play is based on a lightweight, stateless, web-friendly architecture.  Built on Akka, Play provides predictable and minimal resource comsumption (CPU, memory, threads) for highly-scalable applications.  This app will teach you how to start building Play 2.1 apps with Java and Scala.""",
-        tags = Seq("play", "java", "scala", "starter")))
+        tags = Seq("Introduction", "play", "java", "scala", "starter")))
 
   private val index: Map[String, TemplateMetadata] = (metadata map (m => m.id -> m)).toMap
   override def template(id: String): Option[Template] =
@@ -113,7 +118,7 @@ class DemoTemplateCache() extends TemplateCache {
     for {
       m <- metadata
       // Hack so we can search this stuff.
-      searchstring = s"""${m.name} ${m.tags mkString " "} ${m.description} ${m.version}"""
+      searchstring = s"""${m.title} ${m.name} ${m.tags mkString " "} ${m.description}"""
       // TODO - better search
       if searchstring contains query
     } yield m
