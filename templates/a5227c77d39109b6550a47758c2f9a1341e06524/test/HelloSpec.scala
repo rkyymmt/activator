6953817e51d1673a3b5df92561e1542144b5af29
Josh Suereth <joshua.suereth@gmail.com>
2013-04-24 13:35:18 -0400

    Cut #1 at revamping Hello world Scala tutorial to include more Scala-teaching and less Builder.

--- templates/a5227c77d39109b6550a47758c2f9a1341e06524/test/HelloSpec.scala
+++ templates/a5227c77d39109b6550a47758c2f9a1341e06524/test/HelloSpec.scala
@@ -2,8 +2,7 @@ import org.scalatest._
 import org.scalatest.matchers.ShouldMatchers
 
 class HelloSpec extends FlatSpec with ShouldMatchers {
-  "Hello" should "exist" in {
-    val hello = Hello
-    hello should not be null
+  "Hello" should "have tests" in {
+    true should be(true)
   }
 }
