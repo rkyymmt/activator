75c071375766d0b0d8eeef4adbe82c535df14cff
Josh Suereth <joshua.suereth@gmail.com>
2013-04-25 22:12:38 -0400

    Missed this spelling fix for Heiko.

--- templates/a5227c77d39109b6550a47758c2f9a1341e06524/test/HelloSpec.scala
+++ templates/a5227c77d39109b6550a47758c2f9a1341e06524/test/HelloSpec.scala
@@ -3,6 +3,6 @@ import org.scalatest.matchers.ShouldMatchers
 
 class HelloSpec extends FlatSpec with ShouldMatchers {
   "Hello" should "have tests" in {
-    true should be(true)
+    true should be === true
   }
 }
