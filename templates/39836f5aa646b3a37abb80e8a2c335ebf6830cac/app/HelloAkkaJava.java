42c8dadfc5fc1fb807f2c2e2485d21dd50a1a07e
Jonas Bonér <jonas@jonasboner.com>
2013-04-26 12:47:59 +0200

    Minor changes to hello-akka tutorial

--- templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaJava.java
+++ templates/39836f5aa646b3a37abb80e8a2c335ebf6830cac/app/HelloAkkaJava.java
@@ -28,9 +28,11 @@ public class HelloAkkaJava {
         public void onReceive(Object message) {
             if (message instanceof WhoToGreet)
                 greeting = "hello, " + ((WhoToGreet) message).who;
+
             else if (message instanceof Greet)
                 // Send the current greeting back to the sender
                 getSender().tell(new Greeting(greeting), getSelf());
+
             else unhandled(message);
         }
     }
