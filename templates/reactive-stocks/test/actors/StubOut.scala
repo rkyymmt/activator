a330e72bb9230543ddaef9b2d2d6e5b8b46a4c9a
wsargent <will.sargent@gmail.com>
2013-05-02 16:27:54 -0700

    Remove @author tag.

--- templates/reactive-stocks/test/actors/StubOut.scala
+++ templates/reactive-stocks/test/actors/StubOut.scala
@@ -3,12 +3,6 @@ package actors
 import play.mvc.WebSocket
 import org.codehaus.jackson.JsonNode
 
-/**
- *
- * @author wsargent
- * @since 5/2/13
- */
-
 class StubOut() extends WebSocket.Out[JsonNode]() {
   var expected:JsonNode = null
 
