6abdfa07fbc8ab6a0f0ef77a17a377d62f74de39
Josh Suereth <joshua.suereth@gmail.com>
2013-05-07 10:00:13 -0400

    Now we create the index.db zip file, as well as the cache.properties file that remembers the current hash of the repository.  We can improve this with more information later.

--- io/src/main/scala/snap/IO.scala
+++ io/src/main/scala/snap/IO.scala
@@ -3,6 +3,9 @@ package snap
 import java.io._
 import java.nio.channels._
 import java.nio.charset.Charset
+import java.util.jar.{ Attributes, JarEntry, JarFile, JarInputStream, JarOutputStream, Manifest }
+import java.util.zip.{ CRC32, GZIPOutputStream, ZipEntry, ZipFile, ZipInputStream, ZipOutputStream }
+import scala.collection.immutable.TreeSet
 // HACK NOTICE - This is mimicked from SBT until we can replace it.
 
 object IO {
@@ -228,4 +231,160 @@ object IO {
       }
     } else sys.error("String cannot be encoded by charset " + charset.name)
 
+  /**
+   * Creates a zip file.
+   * @param sources The files to include in the zip file paired with the entry name in the zip.  Only the pairs explicitly listed are included.
+   * @param outputZip The file to write the zip to.
+   */
+  def zip(sources: Traversable[(File, String)], outputZip: File): Unit =
+    archive(sources.toSeq, outputZip, None)
+
+  private def archive(sources: Seq[(File, String)], outputFile: File, manifest: Option[Manifest]) {
+    if (outputFile.isDirectory)
+      error("Specified output file " + outputFile + " is a directory.")
+    else {
+      val outputDir = outputFile.getParentFile
+      createDirectory(outputDir)
+      withZipOutput(outputFile, manifest) { output =>
+        val createEntry: (String => ZipEntry) = if (manifest.isDefined) new JarEntry(_) else new ZipEntry(_)
+        writeZip(sources, output)(createEntry)
+      }
+    }
+  }
+  private def writeZip(sources: Seq[(File, String)], output: ZipOutputStream)(createEntry: String => ZipEntry) {
+    val files = sources.flatMap { case (file, name) => if (file.isFile) (file, normalizeName(name)) :: Nil else Nil }
+
+    val now = System.currentTimeMillis
+    // The CRC32 for an empty value, needed to store directories in zip files
+    val emptyCRC = new CRC32().getValue()
+
+    def addDirectoryEntry(name: String) {
+      output putNextEntry makeDirectoryEntry(name)
+      output.closeEntry()
+    }
+
+    def makeDirectoryEntry(name: String) =
+      {
+        //			log.debug("\tAdding directory " + relativePath + " ...")
+        val e = createEntry(name)
+        e setTime now
+        e setSize 0
+        e setMethod ZipEntry.STORED
+        e setCrc emptyCRC
+        e
+      }
+
+    def makeFileEntry(file: File, name: String) =
+      {
+        //			log.debug("\tAdding " + file + " as " + name + " ...")
+        val e = createEntry(name)
+        e setTime file.lastModified
+        e
+      }
+    def addFileEntry(file: File, name: String) {
+      output putNextEntry makeFileEntry(file, name)
+      transfer(file, output)
+      output.closeEntry()
+    }
+
+    //Calculate directories and add them to the generated Zip
+    allDirectoryPaths(files) foreach addDirectoryEntry
+
+    //Add all files to the generated Zip
+    files foreach { case (file, name) => addFileEntry(file, name) }
+  }
+  private def withZipOutput(file: File, manifest: Option[Manifest])(f: ZipOutputStream => Unit) {
+    Using.fileOutputStream(false)(file) { fileOut =>
+      val (zipOut, ext) =
+        manifest match {
+          case Some(mf) =>
+            import Attributes.Name.MANIFEST_VERSION
+            val main = mf.getMainAttributes
+            if (!main.containsKey(MANIFEST_VERSION))
+              main.put(MANIFEST_VERSION, "1.0")
+            (new JarOutputStream(fileOut, mf), "jar")
+          case None => (new ZipOutputStream(fileOut), "zip")
+        }
+      try f(zipOut)
+      finally zipOut.close
+    }
+  }
+  private def normalizeName(name: String) = {
+    val sep = File.separatorChar
+    if (sep == '/') name else name.replace(sep, '/')
+  }
+
+  // produce a sorted list of all the subdirectories of all provided files
+  private def allDirectoryPaths(files: Iterable[(File, String)]) =
+    TreeSet[String]() ++ (files flatMap { case (file, name) => directoryPaths(name) })
+
+  // map a path a/b/c to List("a/b/", "a/")
+  private def directoryPaths(path: String): List[String] =
+    directories(relativeComponents(path)).filter(_.length > 1)
+
+  // map a path a/b/c to List("a", "b")
+  private def relativeComponents(path: String): List[String] =
+    path.split("/").toList.dropRight(1)
+  // map components List("a", "b", "c") to List("a/b/c/", "a/b/", "a/", "")
+  private def directories(path: List[String]): List[String] =
+    path.foldLeft(List(""))((e, l) => (e.head + l + "/") :: e)
+
+  /**
+   * Copies the contents of the input file `in` to the `out` stream.
+   * The output stream is not closed by this method.
+   */
+  def transfer(in: File, out: OutputStream): Unit =
+    Using.fileInputStream(in) { in => transfer(in, out) }
+
+  /**
+   * Copies all bytes from the given input stream to the given output stream.
+   * Neither stream is closed.
+   */
+  def transfer(in: InputStream, out: OutputStream): Unit = transferImpl(in, out, false)
+
+  private def transferImpl(in: InputStream, out: OutputStream, close: Boolean) {
+    try {
+      val buffer = new Array[Byte](8192)
+      def read(): Unit = {
+        val byteCount = in.read(buffer)
+        if (byteCount >= 0) {
+          out.write(buffer, 0, byteCount)
+          read()
+        }
+      }
+      read()
+    } finally { if (close) in.close }
+  }
+}
+
+abstract class Using[Source, T] {
+  protected def open(src: Source): T
+  def apply[R](src: Source)(f: T => R): R =
+    {
+      val resource = open(src)
+      try { f(resource) }
+      finally { close(resource) }
+    }
+  protected def close(out: T): Unit
+}
+trait OpenFile[T] extends Using[File, T] {
+  protected def openImpl(file: File): T
+  protected final def open(file: File): T =
+    {
+      val parent = file.getParentFile
+      if (parent != null)
+        IO.createDirectory(parent)
+      openImpl(file)
+    }
+}
+object Using {
+  def file[T <: Closeable](openF: File => T): OpenFile[T] = file(openF, closeCloseable)
+  def file[T](openF: File => T, closeF: T => Unit): OpenFile[T] =
+    new OpenFile[T] {
+      def openImpl(file: File) = openF(file)
+      def close(t: T) = closeF(t)
+    }
+  private def closeCloseable[T <: Closeable]: T => Unit = _.close()
+  def fileInputStream = file(f => new BufferedInputStream(new FileInputStream(f)))
+  def fileOutputStream(append: Boolean = false) = file(f => new BufferedOutputStream(new FileOutputStream(f, append)))
 }
