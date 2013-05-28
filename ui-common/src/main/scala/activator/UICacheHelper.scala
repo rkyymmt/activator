1c21104897cb0b647dcf68f1194528d4d60b3339
Josh Suereth <joshua.suereth@gmail.com>
2013-05-28 10:56:38 -0400

    Cache repo now pulls templates from remote repo when needed.

--- ui-common/src/main/scala/activator/UICacheHelper.scala
+++ ui-common/src/main/scala/activator/UICacheHelper.scala
@@ -6,14 +6,20 @@ import activator.cache._
 import activator.cache.Actions.cloneTemplate
 import akka.actor.ActorRefFactory
 import java.io.File
+import activator.cache.RemoteTemplateRepository
+import com.typesafe.config.ConfigFactory
 
 // This helper constructs the template cache in the default CLI/UI location.
 object UICacheHelper {
   def makeDefaultCache(actorFactory: ActorRefFactory)(implicit timeout: akka.util.Timeout): TemplateCache = {
+    // TODO - Config or ActiavtorProperties?
+    val config = ConfigFactory.load()
+    val remote = RemoteTemplateRepository(config)
+
     DefaultTemplateCache(
       actorFactory = actorFactory,
       location = new File(ActivatorProperties.ACTIVATOR_TEMPLATE_CACHE),
-      //remote=TODO
+      remote = remote,
       seedRepository = Option(ActivatorProperties.ACTIVATOR_TEMPLATE_LOCAL_REPO) map (new File(_)) filter (_.isDirectory))
   }
 }
