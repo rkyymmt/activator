dbbe2281fc0a0c1b2ae40862fb09dbb7f2e7fd21
Josh Suereth <joshua.suereth@gmail.com>
2013-05-31 13:08:48 -0400

    Fix misisng log parameter.

--- ui-common/src/main/scala/activator/UICacheHelper.scala
+++ ui-common/src/main/scala/activator/UICacheHelper.scala
@@ -8,17 +8,22 @@ import akka.actor.ActorRefFactory
 import java.io.File
 import activator.cache.RemoteTemplateRepository
 import com.typesafe.config.ConfigFactory
+import akka.actor.ActorSystem
+import akka.actor.ActorContext
 
 // This helper constructs the template cache in the default CLI/UI location.
 object UICacheHelper {
   def makeDefaultCache(actorFactory: ActorRefFactory)(implicit timeout: akka.util.Timeout): TemplateCache = {
     // TODO - Config or ActiavtorProperties?
     val config = ConfigFactory.load()
-    val remote = RemoteTemplateRepository(config)
+    val log = actorFactory match {
+      case system: ActorSystem => system.log
+      case context: ActorContext => context.system.log
+      case whatever => throw new RuntimeException(s"don't know how to get log from $whatever")
+    }
+    val remote = RemoteTemplateRepository(config, log)
     val localCache = new File(ActivatorProperties.ACTIVATOR_TEMPLATE_CACHE)
     val localSeed = Option(ActivatorProperties.ACTIVATOR_TEMPLATE_LOCAL_REPO) map (new File(_)) filter (_.isDirectory)
-    //println("Creating template cache @ " + localCache.getAbsolutePath)
-    //println("Local repo seed @ " + localSeed.map(_.getAbsolutePath))
     DefaultTemplateCache(
       actorFactory = actorFactory,
       location = localCache,
