a64027dde81c1071ee98c4c4f79b988acfa94458
Havoc Pennington <hp@pobox.com>
2013-06-07 21:33:41 -0400

    Improve toString on App

--- ui/app/snap/App.scala
+++ ui/app/snap/App.scala
@@ -8,7 +8,12 @@ import scala.util.control.NonFatal
 import java.net.URLEncoder
 
 class App(val config: AppConfig, val system: ActorSystem, val sbtProcessLauncher: SbtProcessLauncher) {
-  val actor = system.actorOf(Props(new AppActor(config, sbtProcessLauncher)), name = "app-" + URLEncoder.encode(config.id, "UTF-8"))
+  val appInstance = App.nextInstanceId.getAndIncrement()
+  override def toString = s"App(${config.id}@$appInstance})"
+  val actorName = "app-" + URLEncoder.encode(config.id, "UTF-8") + "-" + appInstance
+
+  val actor = system.actorOf(Props(new AppActor(config, sbtProcessLauncher)),
+    name = actorName)
 
   // TODO - this method is dangerous, as it hits the file system.
   // Figure out when it should initialize/run.
@@ -20,4 +25,9 @@ class App(val config: AppConfig, val system: ActorSystem, val sbtProcessLauncher
     } catch {
       case e: java.io.IOException => None // TODO - Log?
     }
+
+}
+
+object App {
+  val nextInstanceId = new AtomicInteger(1)
 }
