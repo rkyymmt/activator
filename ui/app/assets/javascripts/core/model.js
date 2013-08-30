5c987a650dd3b03cbfcdc969f8482004007db786
Havoc Pennington <hp@pobox.com>
2013-08-30 13:07:25 -0400

    Move all the code for 'model' into the model module
    Remove $.extend hackery.

--- ui/app/assets/javascripts/core/model.js
+++ ui/app/assets/javascripts/core/model.js
@@ -1,4 +1,4 @@
-define(['./pluginapi'], function(api) {
+define(['./pluginapi', './router', './tutorial/tutorial', './navigation'], function(api, router, Tutorial, navigation) {
 
 	var ko = api.ko;
 
@@ -6,6 +6,10 @@ define(['./pluginapi'], function(api) {
 	// so that this first part is available during construction
 	// of the second part.
 	var model = {
+		api: api,
+		plugins: null, // filled in by init
+		router: router,
+		tutorial: new Tutorial(),
 		snap: {
 			// TODO - replace uses of this with app.name() below
 			appName: window.serverAppModel.name ? window.serverAppModel.name : window.serverAppModel.id,
@@ -19,6 +23,22 @@ define(['./pluginapi'], function(api) {
 				hasPlay: ko.observable(false),
 				hasConsole: ko.observable(false)
 			}
+		},
+		// This is the initialization of the application...
+		init: function(plugins) {
+			var self = this;
+			self.widgets = [];
+			self.plugins = plugins;
+			// TODO - initialize plugins in a better way perhaps...
+			$.each(self.plugins.list, function(idx,plugin) {
+				self.router.registerRoutes(plugin.routes);
+				$.each(plugin.widgets, function(idx, widget) {
+					self.widgets.push(widget);
+				});
+			});
+			self.router.init();
+			ko.applyBindings(self, window.body);
+			navigation.init();
 		}
 	};
 
